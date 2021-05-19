a9bbeab9f4a562bfb4ce4d84b1c945f3c8a970be
Yoshiyuki Takeda <andysumi@ruby.net.tokai-u.jp>
2021-05-19 22:36:55 +0900

    getSpecificDatabaseメソッドを追加

--- src/Notion.js
+++ src/Notion.js
@@ -15,4 +15,15 @@ class Notion { // eslint-disable-line
     if (startCursor) param['start_cursor'] = startCursor;
     return this.client_.fetchGet('/databases', param);
   }
+
+  /**
+   * 指定したデータベースを取得する
+   * https://developers.notion.com/reference/get-database
+   * @param {string} databaseId データベースのID
+   * @return {Object} 処理結果
+   */
+  getSpecificDatabase(databaseId) {
+    if (!databaseId) throw new Error('"databaseId" must be specified');
+    return this.client_.fetchGet(`/databases/${databaseId}`);
+  }
 }
--- src/index.js
+++ src/index.js
@@ -10,10 +10,21 @@ function create(token, version) { // eslint-disable-line no-unused-vars
 
 /**
  * データベースを取得する
+ * https://developers.notion.com/reference/get-databases
  * @param {number} pageSize リストの項目数、最大:100
  * @param {string} startCursor 指定されたカーソルの後に始まる結果のページを返す
- * @return {Object} データベースのリスト
+ * @return {Object} 処理結果
  */
 function getDatabases(pageSize, startCursor) { // eslint-disable-line no-unused-vars
+  throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
+}
+
+/**
+ * 指定したデータベースを取得する
+ * https://developers.notion.com/reference/get-database
+ * @param {string} databaseId データベースのID
+ * @return {Object} 処理結果
+ */
+function getSpecificDatabase(databaseId) { // eslint-disable-line no-unused-vars
   throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
 }
